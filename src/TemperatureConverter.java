29a9622eb0c222e93e64cc7b05379acb9e84188a
GabrielFaglioni <gabriel.faglioni@usp.br>
2022-10-21 21:23:07 -0300

    Added Validation to the inputs

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,13 +1,35 @@
 import java.util.Scanner;
-
+import java.util.InputMismatchException;
 public class TemperatureConverter {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Quantas temperaturas gostaria de converter?");
 
-        //TODO: IMPLEMENTAR TRY CATCH
-        int numTemp = input.nextInt();
+        Integer numTemp = 0;
+
+        do {
+            while (!input.hasNextInt()){
+                System.out.println("Favor escolher um valor INTEIRO maior que zero: ");
+                input.next();
+            }
+            numTemp = input.nextInt();
+            if(numTemp <= 0){
+                System.out.println("Favor escolher um valor INTEIRO maior que zero: ");
+            }
+        } while (numTemp <= 0);
+
+//        do {
+//            try {
+//                numTemp = input.nextInt();
+//            } catch (Exception exception) {
+//                System.out.println("Erro no valor de entrada : " + exception);
+//                System.out.print("Favor escolher um valor Inteiro maior que zero: ");
+//            }
+//        } while (!input.hasNextInt());
+
+
+
 
         System.out.println("Agora escolha duas unidades de temperatura:");
         System.out.println("C: Celcius [ºC]");
@@ -15,11 +37,12 @@ public class TemperatureConverter {
         System.out.println("K: Kelvin [K]");
         System.out.print("Unidade de origem (C, F ou K):");
 
+
         //TODO: Implementar para não validar String
         char unidadeOrigem = input.next().charAt(0);
 
         while(unidadeOrigem != 'C' && unidadeOrigem != 'F' && unidadeOrigem != 'K'){
-            System.out.print("Favor digitar uma unidade de origem (C, F ou K):");
+            System.out.print("Favor digitar uma unidade de origem (C, F ou K): ");
             unidadeOrigem = input.next().charAt(0);
         }
 
@@ -27,21 +50,27 @@ public class TemperatureConverter {
         char unidadeDesejada = input.next().charAt(0);
 
         while(unidadeDesejada != 'C' && unidadeDesejada != 'F' && unidadeDesejada != 'K'){
-            System.out.print("Favor digitar uma unidade de origem (C, F ou K):");
+            System.out.print("Favor digitar uma unidade de origem (C, F ou K): ");
             unidadeDesejada = input.next().charAt(0);
         }
 
         // Classe que possui métodos de apoio para facilitar a construção de strings.
         StringHelper stringHelper = new StringHelper();
 
-        System.out.println("Agora insira " + numTemp + " temperaturas (em " + stringHelper.checkUnitOfMeasureName(unidadeOrigem) + "):");
+        System.out.println("Agora insira " + numTemp + " temperaturas (em " + stringHelper.checkUnitOfMeasureName(unidadeOrigem) + "): ");
 
         // Obtém as temperaturas de Entrada
         Double[] temperaturasEntrada = new Double[numTemp];
         Double tempInserida = 0.0;
         for (int i = 0; i < numTemp; i++){
-            //TODO: IMPLEMENTAR TRY CATCH
+
+            System.out.print("Temperatura " + String.valueOf(i+1) + ": ");//tempInserida = input.nextDouble();
+            while (!input.hasNextDouble()){
+                System.out.println("Favor escolher um valor Numérico para a temperatura "+ String.valueOf(i+1) + ": ");
+                input.next();
+            }
             tempInserida = input.nextDouble();
+
             temperaturasEntrada[i] = tempInserida;
         }
 
@@ -54,7 +83,6 @@ public class TemperatureConverter {
         TemperatureConverterCalculator tempConvCalc = new TemperatureConverterCalculator();
 
         // Realiza a conversão das temperaturas
-        System.out.println(conversorEscolhido);
         Double[] temperaturasConvertidas = switch (conversorEscolhido) {
             // Celsius -> Celcius
             case "CC" -> tempConvCalc.celciusToCelcius(temperaturasEntrada);
@@ -79,12 +107,18 @@ public class TemperatureConverter {
 
         // Imprime todas as temperaturas escolhidas e suas respectivas conversões.
         for (int j = 0; j < temperaturasEntrada.length; j++){
-            System.out.println(temperaturasEntrada[j] + stringHelper.checkUnitOfMeasure(unidadeOrigem) + " para " + stringHelper.checkUnitOfMeasureName(unidadeDesejada) + " = " + temperaturasConvertidas[j] + stringHelper.checkUnitOfMeasure(unidadeDesejada));
+//            System.out.println(temperaturasEntrada[j] + stringHelper.checkUnitOfMeasure(unidadeOrigem) + " para " +
+//                    stringHelper.checkUnitOfMeasureName(unidadeDesejada) + " = " + temperaturasConvertidas[j] +
+//                    stringHelper.checkUnitOfMeasure(unidadeDesejada));
+
+            System.out.printf("%.3f" + stringHelper.checkUnitOfMeasure(unidadeOrigem) +
+                    " para " + stringHelper.checkUnitOfMeasureName(unidadeDesejada) +
+                    " = %.3f" + stringHelper.checkUnitOfMeasure(unidadeDesejada) + "\n", temperaturasEntrada[j], temperaturasConvertidas[j]);
         }
 
         ArrayArithmetics dadosTemperaturasEntrada = new ArrayArithmetics(temperaturasEntrada);
         ArrayArithmetics dadosTemperaturasConvertidas = new ArrayArithmetics(temperaturasConvertidas);
-        System.out.print("Média das temperaturas de entrada: " + dadosTemperaturasEntrada.findArrayAverage() + stringHelper.checkUnitOfMeasure(unidadeOrigem));
-        System.out.print("Média das temperaturas convertidas: " + dadosTemperaturasConvertidas.findArrayAverage() + stringHelper.checkUnitOfMeasure(unidadeDesejada));
+        System.out.printf("Média das temperaturas de entrada: %.3f" + stringHelper.checkUnitOfMeasure(unidadeOrigem)+"\n",dadosTemperaturasEntrada.findArrayAverage());
+        System.out.printf("Média das temperaturas convertidas: %.3f" + stringHelper.checkUnitOfMeasure(unidadeDesejada),dadosTemperaturasConvertidas.findArrayAverage());
     }
 }
